699e213048bb3db5612d76b69bb6fc2a00081691
dro14 <and-rey-2309@mail.ru>
2016-03-15 21:01:17 +0300

    Minor clean up

--- game_21.rb
+++ game_21.rb
@@ -4,13 +4,15 @@ user_value_1 = rand(6..11)
 user_value_2 = rand(2..4)
 user_score = user_value_1 + user_value_2
 
+puts "You get 2 cards: #{user_value_1} and #{user_value_2}"
+puts "Final score: #{user_score}"
+
 croupier_value_1 = rand(6..11)
 croupier_value_2 = rand(2..4)
 croupier_score = croupier_value_1 + croupier_value_2
 
-puts "Croupier gets 2 cards: #{croupier_value_1} and #{croupier_value_2}."
+puts "Croupier gets 2 cards: #{croupier_value_1} and #{croupier_value_2}"
 puts "He`s final score: #{croupier_score}"
-puts "You get 2 cards: #{user_value_1} and #{user_value_2}. Final score: #{user_score}"
 
 if user_score > croupier_score
   puts "You won"
