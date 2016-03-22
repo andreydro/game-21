594a935400ceada3ac13510d3c949d5851eb3ee0
dro14 <and-rey-2309@mail.ru>
2016-03-22 21:30:51 +0300

    Add cards array. Add get_top_card method. User takes cards. Croupier takes cards.

--- game_21.rb
+++ game_21.rb
@@ -1,45 +1,67 @@
 puts "Game starts"
 
-user_value_1 = rand(6..11)
-user_value_2 = rand(2..4)
+cards = [
+  2, 3, 4, 6, 7, 8, 9, 10, 11
+] * 4
+
+cards.shuffle!
+
+def get_top_card(cards)
+  cards.pop
+end
+
+user_value_1 = get_top_card(cards)
+user_value_2 = get_top_card(cards)
 user_score = user_value_1 + user_value_2
 
 puts "You get 2 cards: #{user_value_1} and #{user_value_2}"
 puts "Your final score: #{user_score}"
 
-croupier_value_1 = rand(6..11)
-croupier_value_2 = rand(2..4)
+croupier_value_1 = get_top_card(cards)
+croupier_value_2 = get_top_card(cards)
 croupier_score = croupier_value_1 + croupier_value_2
 
 puts "Croupier also gets 2 cards: #{croupier_value_1} and #{croupier_value_2}"
 puts "He`s final score is: #{croupier_score}"
 
-puts "Do you want take one more card?"
-user_answer = gets.chomp
+while true
+  puts "Do you want take one more card?"
+  user_answer = gets.chomp
 
-if user_answer == "yes"
-  user_value_3 = rand(2..4)
-  puts "Your card is: #{user_value_3}"
-  user_score += user_value_3
-  puts "Your final score: #{user_score} "
+  if user_answer == "yes"
+    user_value_3 = get_top_card(cards)
+    puts "Your card is: #{user_value_3}"
+    user_score += user_value_3
+    puts "Your final score: #{user_score}"
+  else
+    break
+  end
 end
 
-puts "Does croupier take one more card?"
-croupier_answer = rand(1..2)
+while true
+  puts "Does croupier take one more card?"
+  croupier_answer = rand(1..2)
 
-if croupier_answer == 1
-  croupier_value_3 = rand(2..4)
-  puts "Croupier card is: #{croupier_value_3}"
-  croupier_score += croupier_value_3
-  puts "Croupier final score: #{croupier_score}"
+  if croupier_answer == 1
+    croupier_value_3 = get_top_card(cards)
+    puts "Croupier card is: #{croupier_value_3}"
+    croupier_score += croupier_value_3
+    puts "Croupier final score: #{croupier_score}"
+  else
+    break
+  end
 end
 
-if user_score > croupier_score
-  puts "Your final score: #{user_score}"
-  puts "Croupier`s final score: #{croupier_score}"
-  puts "You won"
+if user_score <= 21
+  if user_score > croupier_score
+    puts "Your final score: #{user_score}"
+    puts "Croupier`s final score: #{croupier_score}"
+    puts "You won"
+  else
+    puts "Your final score: #{user_score}"
+    puts "Croupier`s final score: #{croupier_score}"
+    puts "You lose"
+  end
 else
-  puts "Your final score: #{user_score}"
-  puts "Croupier`s final score: #{croupier_score}"
   puts "You lose"
 end
